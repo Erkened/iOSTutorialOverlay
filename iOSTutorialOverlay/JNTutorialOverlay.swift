aecd22720956bff61e14050391277dbefcae6755
John <audioyltd@gmail.com>
2015-02-23 18:55:19 +0000

    Updated the project to showcase the different ways the tutorial overlays can be used

---
+++ iOSTutorialOverlay/JNTutorialOverlay.swift
@@ -0,0 +1,397 @@
+//
+//  JNTutorialOverlay.swift
+//  iOSTutorialOverlay
+//
+//  Created by Jonathan Neumann on 20/02/2015.
+//  Copyright (c) 2015 Audio Y. All rights reserved.
+//  www.audioy.co
+
+import Foundation
+import UIKit
+
+struct ScreenConstants {
+    
+    static var size: CGRect = UIScreen.mainScreen().bounds
+    static var width = size.width
+    static var height = size.height
+    static var scale:CGFloat = UIScreen.mainScreen().scale
+}
+
+struct ColourConstants {
+    // If the RGB value is 252831, pass 0x252831 as the HEX value
+    
+    static var mercury = getUIColorObjectFromHex("E6E6E6", 1.0) // Normal text colour, light grey
+    static var shark = getUIColorObjectFromHex("1A1C22", 1.0) // Darker background colour, very dark blue
+    static var brightTurquoise = getUIColorObjectFromHex("31D8F8", 1.0) // Bright turquoise colour
+}
+
+enum Corners{
+    case Straight
+    case Rounded
+}
+
+enum Theme{
+    case Dark
+    case Light
+}
+
+// Custom UIView with the name of the overlay (stored in NSUserDefaults)
+class JNTutorialOverlay:UIView{
+    
+    var overlayName:String? // The name saved in NSUserDefaults once the overlay has been removed
+    var opacity:CGFloat?
+    let oneTap = UITapGestureRecognizer()
+    var onHide: ((Bool) -> ())? // This is the completion handler to be called when hideOverlay is called
+    
+    var corners:Corners?{
+        didSet {
+            switch self.corners!{
+                
+            case .Straight:
+                setStraightCorners()
+            case .Rounded:
+                setRoundedCorners()
+            }
+        }
+    }
+    
+    var theme:Theme?{
+        didSet {
+            switch self.theme!{
+                
+            case .Dark:
+                setDarkTheme()
+            case .Light:
+                setLightTheme()
+            }
+        }
+    }
+    
+    private var overlayView:UIView?
+    private var titleLabel:UILabel?
+    private var messageLabel:UILabel?
+    private var imageView:UIImageView?
+    
+    var title:String?{
+        didSet{
+            self.titleLabel?.text = self.title!
+        }
+    }
+    
+    var message:String?{
+        didSet{
+            if let message = message{
+                setMessageLabel(message)
+            }
+        }
+    }
+    
+    var image:UIImage?{
+        didSet{
+            if let image = self.image{
+                setImageView(image)
+            }
+        }
+    }
+    
+    /* -------- INIT METHODS -------- */
+    
+    // Init with a centred frame and no picture
+    init(overlayName:String, width:CGFloat, height:CGFloat, opacity: CGFloat, title:String?, message:String?){
+        
+        super.init(frame: CGRectMake(0, 0, ScreenConstants.width, ScreenConstants.height))
+        
+        // Centre the frame within the screen
+        //super.init(frame:CGRectMake((ScreenConstants.width - width)/2, (ScreenConstants.height - height)/2, width, height))
+        // Create the overlay and centre it within the screen
+        createOverlay(CGRectMake((ScreenConstants.width - width)/2, (ScreenConstants.height - height)/2, width, height), overlayName: overlayName, opacity: opacity, title: title, message: message, image: nil)
+    }
+    
+    // Init with a centred frame and a picture
+    init(overlayName:String, width:CGFloat, height:CGFloat, opacity: CGFloat, title:String?, message:String?, image:UIImage?){
+        
+        super.init(frame: CGRectMake(0, 0, ScreenConstants.width, ScreenConstants.height))
+        // Centre the frame within the screen
+        //super.init(frame:CGRectMake((ScreenConstants.width - width)/2, (ScreenConstants.height - height)/2, width, height))
+        // Create the overlay and centre it within the screen
+        createOverlay(CGRectMake((ScreenConstants.width - width)/2, (ScreenConstants.height - height)/2, width, height), overlayName: overlayName, opacity: opacity, title: title, message: message, image: image)
+    }
+    
+    // Init with a set frame and no picture
+    init(overlayName:String, frame:CGRect, opacity:CGFloat, title:String?, message:String?) {
+        
+        super.init(frame: CGRectMake(0, 0, ScreenConstants.width, ScreenConstants.height))
+        // Init the frame
+        //super.init(frame: frame)
+        // Create the overlay
+        createOverlay(frame, overlayName: overlayName, opacity: opacity, title: title, message: message, image: nil)
+    }
+    
+    // Init with a set frame and a picture
+    init(overlayName:String, frame:CGRect, opacity:CGFloat, title:String?, message:String?, image:UIImage?) {
+        
+        super.init(frame: CGRectMake(0, 0, ScreenConstants.width, ScreenConstants.height))
+        // Init the frame
+        //super.init(frame: frame)
+        // Create the overlay
+        createOverlay(frame, overlayName: overlayName, opacity: opacity, title: title, message: message, image: image)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /* -------- OVERLAY VIEW METHODS -------- */
+    func createOverlay(frame: CGRect, overlayName:String, opacity: CGFloat, title:String?, message:String?, image:UIImage?){
+        
+        // Init the name
+        self.overlayName = overlayName
+        // Init the opacity
+        self.opacity = opacity
+        // Init the tap to get rid of the overlay
+        oneTap.addTarget(self, action: "hideOverlay:")
+        self.addGestureRecognizer(oneTap)
+        
+        // Prepare the overlay view
+        overlayView = UIView(frame: frame)
+        
+        // Set a dark theme by default
+        setDarkTheme()
+        // Set rounded corners by default
+        setRoundedCorners()
+        
+        // Prepare the title label
+        var font = UIFont(name: "Helvetica-Bold", size: 19.0)
+        titleLabel = UILabel(frame: CGRectMake(10, 20, overlayView!.frame.width - 20, 50))
+        titleLabel?.numberOfLines = 0
+        titleLabel?.textAlignment = .Center
+        titleLabel?.font = font!
+        titleLabel?.textColor = ColourConstants.mercury
+        // Assign it the text if it has been set
+        if let title = title{
+            self.title = title
+            titleLabel?.text = title
+        }
+        
+        // Prepare the message label
+        font = UIFont(name: "Helvetica", size: 16.0)
+        messageLabel = UILabel(frame: CGRectMake(20, 90, overlayView!.frame.width - 40, 0))
+        messageLabel?.numberOfLines = 0
+        messageLabel?.textAlignment = .Center
+        messageLabel?.font = font!
+        messageLabel?.textColor = ColourConstants.mercury
+        // Assign it the message if it has been set
+        if let message = message{
+            self.message = message
+            setMessageLabel(message)
+        }
+        
+        // Prepare the image view
+        self.imageView = UIImageView(frame: CGRectMake(0, 0, 0, 0))
+        if let image = image{
+            self.image = image
+            setImageView(image)
+        }
+        
+        overlayView?.addSubview(titleLabel!)
+        overlayView?.addSubview(messageLabel!)
+        overlayView?.addSubview(imageView!)
+        self.addSubview(overlayView!)
+    }
+    
+    // Use this function to show the view
+    func show(){
+        
+        if checkScreenShownBefore(self.overlayName!) == false{
+            animateWithPopIn()
+        }
+    }
+    
+    // Use this function to show the view and know when it's been hidden thanks to the block
+    func showWithBlock(onHide: (Bool) -> ()){
+        
+        self.onHide = onHide
+        
+        if checkScreenShownBefore(self.overlayName!) == false{
+            animateWithPopIn()
+        }
+        
+    }
+    
+    func hideOverlay(sender:UITapGestureRecognizer){
+        
+        if let name = self.overlayName{
+            // Save in NSUserDefaults as this overlay is not to be shown again
+            //NSUserDefaults.standardUserDefaults().setBool(true, forKey: name)
+            //NSUserDefaults.standardUserDefaults().synchronize()
+        }
+        
+        // Animate
+        animateWithPopOut()
+    }
+    
+    func checkScreenShownBefore(name:String) -> BooleanLiteralType{
+        
+        if let hasBeenShown:AnyObject = NSUserDefaults.standardUserDefaults().objectForKey(name){
+            // If an object was returned, it means that something was shown. No need to create the overlay then!
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    /* -------- SETTING METHODS IN ORDER TO AVOIR REPEATING CODE -------- */
+    func setStraightCorners(){
+        overlayView?.layer.borderWidth = 3
+        overlayView?.layer.cornerRadius = 0
+    }
+    
+    func setRoundedCorners(){
+        overlayView?.layer.borderWidth = 3
+        overlayView?.layer.cornerRadius = 10
+        overlayView?.clipsToBounds = true
+    }
+    
+    func setDarkTheme(){
+        self.backgroundColor = getUIColorObjectFromHex("000000", 0.5)
+        overlayView?.backgroundColor = getUIColorObjectFromHex("000000", opacity!)
+        overlayView?.layer.borderColor = UIColor.blackColor().CGColor
+        self.titleLabel?.textColor = ColourConstants.mercury
+        self.messageLabel?.textColor = ColourConstants.mercury
+    }
+    
+    func setLightTheme(){
+        self.backgroundColor = getUIColorObjectFromHex("FFFFFF", 0.5)
+        overlayView?.backgroundColor = getUIColorObjectFromHex("FFFFFF", opacity!)
+        overlayView?.layer.borderColor = UIColor.whiteColor().CGColor
+        self.titleLabel?.textColor = ColourConstants.shark
+        self.messageLabel?.textColor = ColourConstants.shark
+    }
+    
+    func setMessageLabel(message:String){
+        self.messageLabel?.text = message
+        self.messageLabel?.sizeToFit()
+        self.messageLabel?.frame = CGRectMake(20, 90, overlayView!.frame.width - 40, self.messageLabel!.frame.height)
+        // Move the image if it already exists
+        if let image = self.image{
+            setImageView(image)
+        }
+    }
+    
+    func setImageView(image:UIImage){
+        // Reset the imageView frame to the dimensions of the image
+        self.imageView?.frame.size.width = image.size.width
+        self.imageView?.frame.size.height = image.size.height
+        // Set the frame in the middle
+        self.imageView?.frame.origin.y = messageLabel!.frame.origin.y + messageLabel!.frame.height + 20
+        self.imageView?.frame.origin.x = (overlayView!.frame.width - image.size.width)/2
+        // Finally, set the image
+        self.imageView?.image = self.image!
+    }
+    
+    /* -------- ANIMATION METHODS -------- */
+    func animateWithPopIn(){
+        
+        let rootViewController: UIViewController = UIApplication.sharedApplication().windows[0].rootViewController!!
+        let originalFrame = overlayView!.frame
+        
+        // Start at the centre
+        overlayView!.frame.origin.x = originalFrame.origin.x + originalFrame.width/2
+        overlayView!.frame.size.width = 0
+        overlayView!.frame.origin.y = originalFrame.origin.y + originalFrame.height/2
+        overlayView!.frame.size.height = 0
+        
+        // Hide everything
+        hideElements()
+        
+        rootViewController.view.addSubview(self)
+        
+        // Animate
+        UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut, animations: {
+            
+            // Expand too much!
+            self.overlayView!.frame.origin.x = originalFrame.origin.x - 10
+            self.overlayView!.frame.size.width = originalFrame.width + 20
+            self.overlayView!.frame.origin.y = originalFrame.origin.y - 10
+            self.overlayView!.frame.size.height = originalFrame.height + 20
+            
+            }, completion: { finished in
+                
+                // Shrink back to the intended frame
+                UIView.animateWithDuration(0.1, delay: 0.0, options: .CurveEaseOut, animations: {
+                    
+                    self.overlayView!.frame = originalFrame
+                    self.showElements()
+                    }, nil)
+        })
+    }
+    
+    func animateWithPopOut(){
+        
+        // Hide everything
+        hideElements()
+        
+        // Animate
+        UIView.animateWithDuration(0.1, delay: 0.0, options: .CurveEaseOut, animations: {
+            
+            // Step 1 - Expand a little bit...
+            self.overlayView!.frame.origin.x = self.overlayView!.frame.origin.x - 10
+            self.overlayView!.frame.size.width = self.overlayView!.frame.width + 20
+            self.overlayView!.frame.origin.y = self.overlayView!.frame.origin.y - 10
+            self.overlayView!.frame.size.height = self.overlayView!.frame.height + 20
+            }, completion: { finished in
+                
+                UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut, animations: {
+                    
+                    // Step 2 - Shrink!
+                    self.overlayView!.frame.origin.x = self.overlayView!.frame.origin.x + self.overlayView!.frame.width/2
+                    self.overlayView!.frame.size.width = 0
+                    self.overlayView!.frame.origin.y = self.overlayView!.frame.origin.y + self.overlayView!.frame.height/2
+                    self.overlayView!.frame.size.height = 0
+                    
+                    }, completion: { finished in
+                        self.removeFromSuperview()
+                        self.onHide?(true) // Let the block know the overlay has finished being removed
+                })
+        })
+    }
+    
+    func hideElements(){
+        
+        self.titleLabel?.alpha = 0
+        self.messageLabel?.alpha = 0
+        self.imageView?.alpha = 0
+    }
+    
+    func showElements(){
+        
+        self.titleLabel?.alpha = 1
+        self.messageLabel?.alpha = 1
+        self.imageView?.alpha = 1
+    }
+    
+}
+
+/* -------- CUSTOM METHODS -------- */
+func getUIColorObjectFromHex(hex:String, alpha:CGFloat) -> UIColor {
+    var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
+    
+    if (cString.hasPrefix("#")) {
+        cString = cString.substringFromIndex(advance(cString.startIndex, 1))
+    }
+    
+    if (countElements(cString) != 6) {
+        return UIColor.grayColor()
+    }
+    
+    var rgbValue:UInt32 = 0
+    NSScanner(string: cString).scanHexInt(&rgbValue)
+    
+    return UIColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(alpha)
+    )
+}
--- iOSTutorialOverlay/ViewController.swift
+++ iOSTutorialOverlay/ViewController.swift
@@ -2,9 +2,9 @@
 //  ViewController.swift
 //  iOSTutorialOverlay
 //
-//  Created by John on 20/02/2015.
+//  Created by Jonathan Neumann on 20/02/2015.
 //  Copyright (c) 2015 Audio Y. All rights reserved.
-//
+//  www.audioy.co
 
 import UIKit
 
@@ -13,6 +13,61 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = ColourConstants.brightTurquoise
+        
+        let screen1 = JNTutorialOverlay(overlayName: "Screen1", width: 300, height: 300, opacity: 0.8, title: "Screen 1", message: "This is the default overlay.\n The default overlay comes with a dark theme, rounded corners and is centered, but you can change that.")
+        
+        screen1.showWithBlock(){ hidden in
+            
+            println("Screen 1 has been hidden")
+            
+            let screen2 = JNTutorialOverlay(overlayName: "Screen2", frame: CGRectMake(0, 100, ScreenConstants.width - 100, 200), opacity: 0.6, title: "Screen 2", message: "This one for example does not have a centered frame, and the opacity is less.")
+            
+            screen2.showWithBlock(){ hidden in
+                
+                println("Screen 2 has been hidden")
+                
+                let screen3 = JNTutorialOverlay(overlayName: "Screen3", frame: CGRectMake(100, ScreenConstants.height - 200, ScreenConstants.width - 100, 200), opacity: 0.8, title: "Screen 3", message: "This one also has a custom frame but with a light theme and straight corners.")
+                screen3.theme = .Light
+                screen3.corners = .Straight
+                
+                screen3.showWithBlock(){ hidden in
+                    
+                    println("Screen 3 has been hidden")
+                    
+                    let screen4 = JNTutorialOverlay(overlayName: "Screen4", width: 250, height: 250, opacity: 0.7, title: "Screen 4", message: "Overlays can also include a picture under the text, like this one.", image: UIImage(named: "Birdcast"))
+                    
+                    screen4.showWithBlock(){ hidden in
+                        
+                        println("Screen 4 has been hidden")
+                        
+                        let screen5 = JNTutorialOverlay(overlayName: "Screen5", frame: CGRectMake(0, 200, ScreenConstants.width, 200), opacity: 0.8, title: nil, message: nil)
+                        
+                        screen5.title = "Screen 5"
+                        screen5.message = "Of course all the optional values such as title, message, picture, corners and theme can be modified after the overlay has been created."
+                        screen5.theme = .Light
+                        screen5.corners = .Straight
+                        
+                        screen5.showWithBlock(){ hidden in
+                            
+                            println("Screen 5 has been hidden")
+                            
+                            let screen6 = JNTutorialOverlay(overlayName: "Screen6", width: 300, height: 180, opacity: 0.8, title: "Screen 6", message: "And because these overlays are supposed to be shown to a first-time user only, they will only appear once!")
+                            
+                            screen6.showWithBlock(){ hidden in
+                                
+                                println("Screen 6 has been hidden")
+                                
+                                let screen7 = JNTutorialOverlay(overlayName: "Screen7", width: 300, height: 300, opacity: 0.8, title: "Final screen", message: "You can see these overlays in action in our apps Birdcast and Q-Beat (available for free on the App Store).\n\nWe hope you enjoyed this run-through.\nLet us know if you end up using our tutorial overlays in your apps! :)")
+                                
+                                screen7.show()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
